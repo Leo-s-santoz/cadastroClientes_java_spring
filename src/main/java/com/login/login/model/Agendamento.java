05f4ed0122007ae1d8af886acc32a2a7a35fb6a6
Leo-s-santoz <leonardosantos11080@gmail.com>
2025-02-13 14:54:50 -0300

    classe Agendamento atualizada compondo da classe Cliente

--- src/main/java/com/login/login/model/Agendamento.java
+++ src/main/java/com/login/login/model/Agendamento.java
@@ -3,6 +3,8 @@ package com.login.login.model;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+
 import java.util.Date;
 
 @Entity
@@ -16,9 +18,27 @@ public class Agendamento {
     @JoinColumn(name = "cpf", referencedColumnName = "cpf")
     private Cliente cliente;
 
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "cep", column = @Column(name = "endereco_cep")),
+            @AttributeOverride(name = "logradouro", column = @Column(name = "endereco_logradouro")),
+            @AttributeOverride(name = "numero", column = @Column(name = "endereco_numero")),
+            @AttributeOverride(name = "bairro", column = @Column(name = "endereco_bairro")),
+            @AttributeOverride(name = "estado", column = @Column(name = "endereco_estado")),
+            @AttributeOverride(name = "cidade", column = @Column(name = "endereco_cidade"))
+    })
+    private Endereco endereco;
+
+    @NotEmpty
+    private String telefone;
+
+    @NotEmpty
+    private String whatsapp;
+
     @Temporal(TemporalType.TIMESTAMP)
     private Date dataCriacao;
 
+    @NotEmpty
     @Temporal(TemporalType.TIMESTAMP)
     private Date dataAdendada;
 
@@ -26,6 +46,7 @@ public class Agendamento {
     protected void onCreate() {
         dataCriacao = new Date();
     }
+
 
     //getters and setters
     public Long getId() {
@@ -42,6 +63,33 @@ public class Agendamento {
 
     public void setCliente(Cliente cliente) {
         this.cliente = cliente;
+        this.endereco = cliente.getEndereco();
+        this.telefone = cliente.getTelefone();
+        this.whatsapp = cliente.getWhatsapp();
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getWhatsapp() {
+        return whatsapp;
+    }
+
+    public void setWhatsapp(String whatsapp) {
+        this.whatsapp = whatsapp;
     }
 
     public Date getDataCriacao() {
